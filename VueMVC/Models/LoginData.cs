3bdd5eb0b162e63daefe052e229e09f1575e18cb
Optirius <tahmid95.aquarius@gmail.com>
2022-08-15 17:44:15 +0600

    Add project files.

---
+++ VueMVC/Models/LoginData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VueMVC.Models
+{
+    public class LoginData
+    {
+        public string email {get; set;}
+        public string Password { get; set; }
+    }
+}
